c34b9c110e1d8b275753dd5889904e2124c118be
Lonely sail <664028812@qq.com>
2022-04-08 17:39:34 +0800

    提交代码 增加左侧模块   还没画页面
    提交代码 增加左侧模块   还没画页面

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -18,23 +18,15 @@
               <span>{{ item.title }}</span>
             </template>
             <template v-for="subItem in item.subs">
-              <el-submenu
-                v-if="subItem.subs"
-                :index="subItem.index"
-                :key="subItem.index"
-              >
+              <el-submenu v-if="subItem.subs" :index="subItem.index" :key="subItem.index">
                 <template #title>{{ subItem.title }}</template>
                 <el-menu-item
                   v-for="(threeItem, i) in subItem.subs"
                   :key="i"
                   :index="threeItem.index"
-                >
-                  {{ threeItem.title }}</el-menu-item
-                >
+                >{{ threeItem.title }}</el-menu-item>
               </el-submenu>
-              <el-menu-item v-else :index="subItem.index" :key="subItem.index"
-                >{{ subItem.title }}
-              </el-menu-item>
+              <el-menu-item v-else :index="subItem.index" :key="subItem.index">{{ subItem.title }}</el-menu-item>
             </template>
           </el-submenu>
         </template>
@@ -50,21 +42,21 @@
 </template>
 
 <script>
-import { computed, watch } from "vue";
-import { useStore } from "vuex";
-import { useRoute } from "vue-router";
+import { computed, watch } from 'vue'
+import { useStore } from 'vuex'
+import { useRoute } from 'vue-router'
 export default {
   setup() {
     const items = [
       {
-        icon: "el-icon-lx-home",
-        index: "/dashboard",
-        title: "系统首页",
+        icon: 'el-icon-lx-home',
+        index: '/dashboard',
+        title: '系统首页',
       },
       {
-        icon: "el-icon-lx-cascades",
-        index: "/table",
-        title: "基础表格",
+        icon: 'el-icon-lx-cascades',
+        index: '/table',
+        title: '基础表格',
       },
       // {
       //   icon: "el-icon-lx-cascades",
@@ -72,103 +64,122 @@ export default {
       //   title: "用户管理",
       // },
       {
-        icon: "el-icon-lx-copy",
-        index: "/tabs",
-        title: "tab选项卡",
+        icon: 'el-icon-lx-copy',
+        index: '/tabs',
+        title: 'tab选项卡',
       },
       {
-        icon: "el-icon-lx-calendar",
-        index: "3",
-        title: "表单相关",
+        icon: 'el-icon-lx-calendar',
+        index: '3',
+        title: '表单相关',
         subs: [
           {
-            index: "/form",
-            title: "基本表单",
+            index: '/form',
+            title: '基本表单',
           },
           {
-            index: "/upload",
-            title: "文件上传",
+            index: '/upload',
+            title: '文件上传',
           },
           {
-            index: "4",
-            title: "三级菜单",
+            index: '4',
+            title: '三级菜单',
             subs: [
               {
-                index: "/editor",
-                title: "富文本编辑器",
+                index: '/editor',
+                title: '富文本编辑器',
               },
             ],
           },
         ],
       },
       {
-        icon: "el-icon-lx-calendar",
-        index: "13",
-        title: "用户角色管理",
+        icon: 'el-icon-lx-calendar',
+        index: '13',
+        title: '用户角色管理',
         subs: [
           {
-            index: "/SysUser",
-            title: "用户管理",
+            index: '/SysUser',
+            title: '用户管理',
           },
           {
-            index: "/SysRole",
-            title: "角色管理",
+            index: '/SysRole',
+            title: '角色管理',
           },
         ],
       },
       {
-        icon: "el-icon-lx-emoji",
-        index: "/icon",
-        title: "自定义图标",
-      },
-      {
-        icon: "el-icon-pie-chart",
-        index: "/charts",
-        title: "schart图表",
-      },
-      {
-        icon: "el-icon-lx-global",
-        index: "/i18n",
-        title: "国际化功能",
-      },
-      {
-        icon: "el-icon-lx-warn",
-        index: "7",
-        title: "错误处理",
+        icon: 'el-icon-lx-calendar',
+        index: '14',
+        title: '菜单权限管理',
         subs: [
           {
-            index: "/permission",
-            title: "权限测试",
+            index: '/SysUser',
+            title: '接口管理',
           },
           {
-            index: "/404",
-            title: "404页面",
+            index: '/SysRole',
+            title: '菜单管理',
+          },
+          {
+            index: '/SysRole',
+            title: '权限分配',
           },
         ],
       },
       {
-        icon: "el-icon-lx-redpacket_fill",
-        index: "/donate",
-        title: "支持作者",
+        icon: 'el-icon-lx-emoji',
+        index: '/icon',
+        title: '自定义图标',
       },
-    ];
+      {
+        icon: 'el-icon-pie-chart',
+        index: '/charts',
+        title: 'schart图表',
+      },
+      {
+        icon: 'el-icon-lx-global',
+        index: '/i18n',
+        title: '国际化功能',
+      },
+      {
+        icon: 'el-icon-lx-warn',
+        index: '7',
+        title: '错误处理',
+        subs: [
+          {
+            index: '/permission',
+            title: '权限测试',
+          },
+          {
+            index: '/404',
+            title: '404页面',
+          },
+        ],
+      },
+      {
+        icon: 'el-icon-lx-redpacket_fill',
+        index: '/donate',
+        title: '支持作者',
+      },
+    ]
 
-    const route = useRoute();
+    const route = useRoute()
 
     const onRoutes = computed(() => {
-      return route.path;
-    });
+      return route.path
+    })
 
-    const store = useStore();
-    const collapse = computed(() => store.state.collapse);
+    const store = useStore()
+    const collapse = computed(() => store.state.collapse)
 
     return {
       items,
       onRoutes,
       collapse,
-    };
+    }
   },
-};
+}
 </script>
 
 <style scoped>
